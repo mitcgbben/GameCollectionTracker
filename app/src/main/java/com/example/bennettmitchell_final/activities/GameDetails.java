2635b6ca03b5180555e7fc2dd3176787820a0240
Mitchell Bennett <mitcg.b.bennett@gmail.com>
2025-05-06 18:35:35 -0500

    final

--- app/src/main/java/com/example/bennettmitchell_final/activities/GameDetails.java
+++ app/src/main/java/com/example/bennettmitchell_final/activities/GameDetails.java
@@ -178,7 +178,7 @@ public class GameDetails extends AppCompatActivity implements AdapterView.OnItem
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         Status newStatus = (Status) statuses.get(position);
         game.setGameStatus(newStatus);
-//        DBManager.updateGame(game);
+        DBManager.updateGame(game); // this line was commented out in the video
 
     }
 
